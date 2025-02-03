09e896f89734ce4e178f816f5de2e44c69101c11
JuanDeuxG <juan.giacometto@deuxit.com>
2025-02-03 17:26:43 -0300

    Switch to hash routing for GitHub Pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'portfolioDelfina';
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        const fragment = this.router.url.split('#')[1];
+        if (fragment) {
+          document.getElementById(fragment)?.scrollIntoView({ behavior: 'smooth' });
+        }
+      }
+    });
+  }
 }
